cc99240ea06b96f7f54c120d834661d72ce19c58
m salman a <mohammadalfarisi6@gmail.com>
2023-03-08 01:27:28 +0700

    add diagnostics and update focus in NvimTree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -6,11 +6,37 @@ end
 
 nvim_tree.setup {
   view = {
-    relativenumber = true
-  }
+    relativenumber = true,
+  },
+  update_focused_file = {
+    enable = true,
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true
+  },
 }
+
+local function open_nvim_tree(data)
+
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 vim.api.nvim_create_autocmd({"QuitPre"}, {
     callback = function() vim.cmd("NvimTreeClose") end,
 })
+
 
