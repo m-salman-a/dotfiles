00ee1afbdc4073df10b97869d1edf603fa1a0d45
m salman a <mohammadalfarisi6@gmail.com>
2023-03-08 01:28:29 +0700

    add more functionalities for flutter-tools

--- lua/user/lsp/flutter-tools.lua
+++ lua/user/lsp/flutter-tools.lua
@@ -7,9 +7,21 @@ end
 local handlers = require("user.lsp.handlers")
 
 flutter_tools.setup {
+  widget_guides = {
+    enabled = true,
+  },
   lsp = {
+    color = {
+      enabled = true,
+    },
     on_attach = handlers.on_attach,
     capabilities = handlers.capabilities,
+    settings = {
+      completeFunctionCalls = true,
+      updateImportsOnRename = true,
+      analysisExcludedFolders = {"$HOME/Developer/flutter"},
+      renameFilesWithClasses = "prompt",
+    }
   }
 }
 
