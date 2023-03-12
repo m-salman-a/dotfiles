fffbacfa971c18c55c63bc9e8ad8ba458a96a84e
m salman a <mohammadalfarisi6@gmail.com>
2023-03-12 19:23:49 +0700

    add keymap for toggling Dart console

--- lua/user/lsp/flutter-tools.lua
+++ lua/user/lsp/flutter-tools.lua
@@ -6,6 +6,21 @@ end
 
 local handlers = require("user.lsp.handlers")
 
+function _DART_TOGGLE_LOG()
+  local wins = vim.api.nvim_list_wins()
+
+  for _, id in pairs(wins) do
+    local bufnr = vim.api.nvim_win_get_buf(id)
+    if vim.api.nvim_buf_get_name(bufnr):match '.*/([^/]+)$' == '__FLUTTER_DEV_LOG__' then
+      return vim.api.nvim_win_close(id, true)
+    end
+  end
+
+  pcall(function()
+    vim.api.nvim_command 'sb + __FLUTTER_DEV_LOG__ | resize 15'
+  end)
+end
+
 flutter_tools.setup {
   lsp = {
     color = {
@@ -13,9 +28,11 @@ flutter_tools.setup {
     },
     on_attach = function(client, bufnr)
       handlers.on_attach(client, bufnr);
+      vim.keymap.set("n", "<Leader>tl", "<cmd>lua _DART_TOGGLE_LOG() <CR>")
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = { "*.dart" },
         callback = function ()
+        callback = function()
           vim.lsp.buf.format { async = false }
         end
       })
