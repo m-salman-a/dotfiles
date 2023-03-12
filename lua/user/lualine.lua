97f42df874312c4be57c9c14f00e9ce429fdc508
m salman a <mohammadalfarisi6@gmail.com>
2023-03-12 19:28:49 +0700

    remove git diff from lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -4,17 +4,6 @@ if not ok then
   return
 end
 
-local function diff_source()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed
-    }
-  end
-end
-
 lualine.setup {
   options = {
     disabled_filetypes = { "NvimTree" }
@@ -35,12 +24,8 @@ lualine.setup {
             return substr
           end
 
-          return str:sub(1, 30)
+          return str
         end
-      },
-      {
-        'diff',
-        source = diff_source
       },
       { 'diagnostics' },
     }
